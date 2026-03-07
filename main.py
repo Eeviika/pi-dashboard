8aa854e5beeb6a6c78c52abc21e6759807d6b8b1
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-03-07 16:39:48 -0500

    Check if base_dir is setup and exit if not

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
-from config import load_config
+from config import load_config, setup_base_dir
 from logging_cfg import setup_logging
 
+if not setup_base_dir():
+    exit(74)
 load_config()
 setup_logging()
 
