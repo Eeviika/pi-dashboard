c54f2ab2f0e9cccb0145827854477d47f4df5269
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-03-07 16:42:16 -0500

    use os_ex* constant instead of magic number when exiting

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import os
+
 from flask import Flask
 
 from config import load_config, setup_base_dir
 from logging_cfg import setup_logging
 
 if not setup_base_dir():
-    exit(74)
+    exit(os.EX_IOERR)
 load_config()
 setup_logging()
 
