7f9b9982c68a4c7813245d555699a91e978f4b02
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-03-07 16:26:58 -0500

    load config and setup logging on main

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import os, psutil, subprocess
-from flask import Flask, jsonify
+from flask import Flask
+
+from config import load_config
+from logging_cfg import setup_logging
+
+load_config()
+setup_logging()
 
 app = Flask(__name__)
