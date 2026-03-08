2413cea52a86f7edc394f35c08a60584da7395e0
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-03-07 20:08:58 -0500

    Added handler to main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
-from pathlib import Path
 
-import psutil
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
 
 from config import load_config, setup_base_dir
 from logging_cfg import setup_logging
+from routers import sysinfo
 
 if not setup_base_dir():
     exit(os.EX_IOERR)
@@ -17,26 +16,7 @@ tea_ml = 0
 
 app = FastAPI()
 
-
-def get_cpu_temp():
-    return Path("/sys/class/thermal/thermal_zone0/temp").read_text().strip()
-
-
-def get_cpu_usage():
-    return psutil.cpu_percent()
-
-
-def get_cpu_freq():
-    return psutil.cpu_freq()
-
-
-def get_cpu_info():
-    return {
-        "temp": get_cpu_temp(),
-        "usage": get_cpu_usage(),
-        "freq": get_cpu_freq()
-    }
-
+app.include_router(sysinfo.router)
 
 @app.get("/api")
 async def get_api():
@@ -62,28 +42,4 @@ async def brew_tea():
     }
 
 
-@app.get("/api/sysinfo")
-async def sysinfo():
-    return {
-        "cpu": get_cpu_info()
-    }
 
-
-@app.get("/api/sysinfo/cpu")
-async def cpu_info():
-    return get_cpu_info()
-
-
-@app.get("/api/sysinfo/cpu/temp")
-async def cpu_temp():
-    return get_cpu_temp()
-
-
-@app.get("/api/sysinfo/cpu/usage")
-async def cpu_usage():
-    return get_cpu_usage()
-
-
-@app.get("/api/sysinfo/cpu/freq")
-async def cpu_freq():
-    return get_cpu_freq()
