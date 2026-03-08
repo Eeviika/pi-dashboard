c5419afce19073d6482ed9ef200c85d70d42aa84
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-03-08 10:56:19 -0400

    Moved teapot api endpoints into a teapot router

--- main.py
+++ main.py
@@ -1,45 +1,26 @@
 import os
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
 
 from config import load_config, setup_base_dir
 from logging_cfg import setup_logging
-from routers import sysinfo
+from routers import sysinfo, teapot
 
 if not setup_base_dir():
     exit(os.EX_IOERR)
 load_config()
 setup_logging()
 
-tea_ml = 0
-
 app = FastAPI()
 
 app.include_router(sysinfo.router)
+app.include_router(teapot.router)
 
 @app.get("/api")
 async def get_api():
     return RedirectResponse(url="/docs")
 
 
-@app.get("/api/coffee", status_code=418)
-async def im_a_teapot():
-    return "I am a teapot and cannot be used to brew coffee."
-
-
-@app.get("/api/tea", status_code=200)
-async def brew_tea():
-    global tea_ml
-
-    if tea_ml >= 100:
-        raise HTTPException(status_code=409, detail="Too much tea, please drain.")
-
-    tea_ml += 10
-    return {
-        "success": True,
-        "current_tea_ml": tea_ml
-    }
-
-
-
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
---
+++ routers/teapot/__init__.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, HTTPException
+
+router = APIRouter(prefix="api/teapot", tags=["System Information", "Memory Information"])
+tea_ml = 0
+
+
+@router.get("/", status_code=418)
+async def root():
+    return "Hello, I am a teapot."
+
+
+@router.get("/coffee", status_code=418)
+async def im_a_teapot():
+    return "I am a teapot and cannot be used to brew coffee."
+
+
+@router.get("/tea", status_code=200)
+async def brew_tea():
+    global tea_ml
+
+    if tea_ml >= 100:
+        raise HTTPException(status_code=418, detail="Too much tea, please drain.")
+
+    tea_ml += 10
+    return {
+        "success": True,
+        "current_tea_ml": tea_ml
+    }
+
+
+@router.get("/drain", status_code=200)
+async def brew_tea():
+    global tea_ml
+
+    if tea_ml == 0:
+        raise HTTPException(status_code=418, detail="There is nothing to drain.")
+
+    tea_ml = 0
+    return {
+        "success": True,
+        "current_tea_ml": tea_ml
+    }
