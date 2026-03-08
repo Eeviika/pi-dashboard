ecbb8aee71beab254e545e568f8c4a0e91bbdda7
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-03-07 19:53:15 -0500

    Added CPU endpoints

--- main.py
+++ main.py
@@ -17,6 +17,27 @@ tea_ml = 0
 
 app = FastAPI()
 
+
+def get_cpu_temp():
+    return Path("/sys/class/thermal/thermal_zone0/temp").read_text().strip()
+
+
+def get_cpu_usage():
+    return psutil.cpu_percent()
+
+
+def get_cpu_freq():
+    return psutil.cpu_freq()
+
+
+def get_cpu_info():
+    return {
+        "temp": get_cpu_temp(),
+        "usage": get_cpu_usage(),
+        "freq": get_cpu_freq()
+    }
+
+
 @app.get("/api")
 async def get_api():
     return RedirectResponse(url="/docs")
@@ -39,3 +60,30 @@ async def brew_tea():
         "success": True,
         "current_tea_ml": tea_ml
     }
+
+
+@app.get("/api/sysinfo")
+async def sysinfo():
+    return {
+        "cpu": get_cpu_info()
+    }
+
+
+@app.get("/api/sysinfo/cpu")
+async def cpu_info():
+    return get_cpu_info()
+
+
+@app.get("/api/sysinfo/cpu/temp")
+async def cpu_temp():
+    return get_cpu_temp()
+
+
+@app.get("/api/sysinfo/cpu/usage")
+async def cpu_usage():
+    return get_cpu_usage()
+
+
+@app.get("/api/sysinfo/cpu/freq")
+async def cpu_freq():
+    return get_cpu_freq()
