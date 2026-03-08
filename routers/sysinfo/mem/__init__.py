84a39bb699d06320882febb6e9ac5c872b991b72
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-03-08 10:19:25 -0400

    Import routers into mem package

--- routers/sysinfo/mem/__init__.py
+++ routers/sysinfo/mem/__init__.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
 
+from . import virtual, swap
+
 router = APIRouter(prefix="/mem", tags=["System Information", "Memory Information"])
+
+router.include_router(virtual.router)
+router.include_router(swap.router)
