cd35704206c36164d44abaea8afc727589ce874e
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-03-08 10:32:45 -0400

    added uptime endpoint

--- routers/sysinfo/__init__.py
+++ routers/sysinfo/__init__.py
@@ -6,9 +6,23 @@ from . import cpu, mem
 router.include_router(cpu.router)
 router.include_router(mem.router)
 
+
+def get_uptime():
+    with open('/proc/uptime', 'r') as f:
+        uptime_seconds = float(f.readline().split()[0])
+
+    return uptime_seconds
+
+
 @router.get("/")
 async def sys_info():
     return {
         "cpu": cpu.get_cpu_info(),
-        "mem": mem.get_memory_info()
+        "mem": mem.get_memory_info(),
+        "uptime": get_uptime()
     }
+
+
+@router.get("/uptime")
+async def uptime():
+    return get_uptime()
