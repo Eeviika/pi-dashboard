363342fd4dc3e2b933d4c5135d156b728ff63a5b
Eeviika <84326942+Eeviika@users.noreply.github.com>
2026-03-07 19:53:05 -0500

    Added /tea and /coffee endpoints

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+from pathlib import Path
 
-from fastapi import FastAPI
+import psutil
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 
 from config import load_config, setup_base_dir
 from logging_cfg import setup_logging
@@ -10,4 +13,29 @@ if not setup_base_dir():
 load_config()
 setup_logging()
 
+tea_ml = 0
+
 app = FastAPI()
+
+@app.get("/api")
+async def get_api():
+    return RedirectResponse(url="/docs")
+
+
+@app.get("/api/coffee", status_code=418)
+async def im_a_teapot():
+    return "I am a teapot and cannot be used to brew coffee."
+
+
+@app.get("/api/tea", status_code=200)
+async def brew_tea():
+    global tea_ml
+
+    if tea_ml >= 100:
+        raise HTTPException(status_code=409, detail="Too much tea, please drain.")
+
+    tea_ml += 10
+    return {
+        "success": True,
+        "current_tea_ml": tea_ml
+    }
